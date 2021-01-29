b076c71deb7e047f644ce5578d800e30aac33d0d
Andres <castroandresrm@gmail.com>
2021-01-29 15:52:49 -0400

    Renombramos los archivos html a php y agregamos los templates

---
+++ includes/templates/footer.php
@@ -0,0 +1,47 @@
+<footer class="site-footer">
+    <div class="contenedor clearfix">
+      <div class="footer-informacion">
+        <h3>Sobre <span>GdlWebCamp</span></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam veritatis, libero nihil dolore voluptate blanditiis unde repudiandae quae voluptatum iure non, vitae commodi fugit reprehenderit enim illo, dolor eum? Nostrum?</p>
+      </div>
+      <div class="ultimos-tweets">
+        <h3>últimos <span>tweets</span></h3>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe veniam assumenda, qui recusandae laudantium a @acm. </li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic fugit ad recusandae quam blanditiis. At explicabo esse voluptas, ea repudiandae sequi. Vero adipisci @officiis tempore neque harum deleniti distinctio repudiandae!</li>
+          <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem error temporibus, dolore, blanditiis quibusdam quas natus saepe repellendus sunt ipsum similique repellat @beatae non inventore aut dolor provident necessitatibus.</li>
+        </ul>
+      </div>
+      <div class="menu">
+        <h3>Redes <span>Sociales</span></h3>
+        <nav class="redes-sociales">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-pinterest"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </nav>
+      </div>
+    </div>
+    <p class="copyright">Todos los Derechos Reservados GDLWEBCAMP 2021 &copy;</p>
+  </footer>
+
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/jquery.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/jquery.animateNumber.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.lettering.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+  
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Oswald&family=PT+Sans&display=swap" rel="stylesheet">
+  
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <header class="site-header">
+    <div class="hero">
+      <div class="contenido-header">
+        <nav class="redes-sociales">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-pinterest"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </nav>
+        <div class="informacion-evento">
+          <div class="clearfix">
+            <p class="fecha"><i class="fas fa-calendar-alt"></i> 16-01-2021</p>
+            <p class="ciudad"><i class="fas fa-map-marker-alt"></i> Cochabamba, Bolivia</p>
+          </div>
+          <h1 class="nombre-sitio">GdlWebCamp</h1>
+          <p class="slogan">La Mejor Conferencia de <span>Diseño Web</span></p>
+        </div>
+      </div>
+    </div>
+  </header><!--Hero-->
+
+  <div class="barra">
+    <div class="contenedor clearfix">
+      <div class="logo">
+        <a href="index.php">
+          <img src="/img/logo.svg" alt="logotipo">
+        </a>
+      </div>
+      <div class="menu-movil">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="navegacion-principal">
+        <a href="conferencia.php">Conferencia</a>
+        <a href="#">Calendario</a>
+        <a href="#">Invitados</a>
+        <a href="registro.php">Reservaciones</a>
+      </nav>
+    </div>
+  </div><!--Barra de Navegacion-->
