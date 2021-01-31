ff6ad553cfbed6ff71584fd89a91f66e5139e271
Andres <castroandresrm@gmail.com>
2021-01-31 10:39:53 -0400

    Creamos el template de invitados

--- index.php
+++ index.php
@@ -72,47 +72,7 @@
     </div>
   </section><!--Programa del evento-->
 
-  <section class="invitados contenedor seccion">
-    <h2>Nuestros Invitados</h2>
-    <ul class="lista-invitados clearfix">
-      <li>
-        <div class="invitado">
-          <img src="img/invitado1.jpg" alt="invitado">
-          <p>Rafael Bautista</p>
-        </div>
-      </li>
-      <li>
-        <div class="invitado">
-          <img src="img/invitado2.jpg" alt="invitado">
-          <p>Shari Herrera</p>
-        </div>
-      </li>
-      <li>
-        <div class="invitado">
-          <img src="img/invitado3.jpg" alt="invitado">
-          <p>Gregorio Sanchez</p>
-        </div>
-      </li>
-      <li>
-        <div class="invitado">
-          <img src="img/invitado4.jpg" alt="invitado">
-          <p>Susana Rivera</p>
-        </div>
-      </li>
-      <li>
-        <div class="invitado">
-          <img src="img/invitado5.jpg" alt="invitado">
-          <p>Harol Garcia</p>
-        </div>
-      </li>
-      <li>
-        <div class="invitado">
-          <img src="img/invitado6.jpg" alt="invitado">
-          <p>Susan Sanchez</p>
-        </div>
-      </li>
-    </ul>
-  </section><!--Seccion invitados-->
+  <?php include_once 'includes/templates/invitados.php'; ?>
 
   <div class="contador parallax">
     <div class="contenedor">
--- invitados.php
+++ invitados.php
@@ -1,37 +1,5 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-    <?php
-    try {
-        require_once('includes/funciones/bd_conexion.php');
-        $sql = "SELECT * FROM `invitados`";
-        $resultado = $conn->query($sql);
-    } catch (\EXception $e) {
-        echo $e->getMessage();
-    }
-    ?>
-    <section class="invitados contenedor seccion">
-        <h2>Nuestros Invitados</h2>
-        <ul class="lista-invitados clearfix">
-            <?php while ($invitados = $resultado->fetch_assoc()) { ?>
-                <li>
-                    <div class="invitado">
-                        <a href="#invitado<?php echo $invitados['id_invitado'];?>" class="invitado-info">
-                            <img src="img/<?php echo $invitados['url_imagen'];?>" alt="invitado">
-                            <p><?php echo $invitados['nombre_invitado']." ".$invitados['apellido_invitado']; ?></p>
-                        </a>
-                    </div>
-                </li>
-                <div style="display: none;">
-                    <div class="invitado-info"  id="invitado<?php echo $invitados['id_invitado'];?>">
-                        <h2><?php echo $invitados['nombre_invitado']." ".$invitados['apellido_invitado']; ?></h2>
-                        <img src="img/<?php echo $invitados['url_imagen'];?>" alt="invitado">
-                        <p><?php echo $invitados['descripcion'];?></p>
-                    </div>
-                </div>
-            <?php } ?>
-        </ul>
-    </section>
-    <!--Seccion invitados-->
-    <?php $conn->close(); ?>
+    <?php include_once 'includes/templates/invitados.php'; ?>
 
 <?php include_once 'includes/templates/footer.php'; ?>
